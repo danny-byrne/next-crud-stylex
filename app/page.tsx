2c484e0ec33590980b55a59544385884432a82e2
Danny Byrne <danny.byrne.dev@gmail.com>
2023-12-26 18:18:13 -0800

    stlye-x compiles, changes not apparent yet

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
 import Link from "next/link";
 
+import * as stylex from "@stylexjs/stylex";
+
+const styles = stylex.create({
+  page: {
+    backgroundColor: "blue",
+    height: "100vh",
+    width: "100vw",
+  },
+  text: {
+    color: "rebeccapurple",
+  },
+});
+
 export default function Page() {
-  return <Link href="/dashboard">Dashboard</Link>;
+  return (
+    <div {...stylex.props(styles.page)}>
+      <div>Hello</div>
+    </div>
+  );
+  // return <Link href="/dashboard">Dashboard</Link>;
 }
